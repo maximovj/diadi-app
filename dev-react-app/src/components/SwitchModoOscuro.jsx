268b2124b2ed39371c7bb6069b5cba794b5b56cc
maximovj <maximovj@outlook.com>
2024-05-19 11:32:09 -0600

    dev-modo-oscuro | *** Se agrega ThemeContext

--- dev-react-app/src/components/SwitchModoOscuro.jsx
+++ dev-react-app/src/components/SwitchModoOscuro.jsx
@@ -1,7 +1,10 @@
+import React, { useContext } from 'react';
 import useModoOscuro from "../hooks/useModoOscuro";
+import { ThemeContext } from "../context/ThemeContext";
 
 export default function SwitchModoOscuro() {
   const [isModoOscuro, toggleModoOscuro] = useModoOscuro();
+  const {theme , toggleTheme } = useContext(ThemeContext);
 
   return (
     <div className="form-check form-switch">
@@ -9,8 +12,8 @@ export default function SwitchModoOscuro() {
         className="form-check-input"
         type="checkbox"
         id="switchModoOscuro"
-        checked={isModoOscuro}
-        onChange={toggleModoOscuro}
+        checked={theme == 'dark'}
+        onChange={toggleTheme}
       />
       <label className="form-check-label" htmlFor="switchModoOscuro">
         Modo oscuro
