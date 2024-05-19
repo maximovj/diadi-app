f9a32a0c74b7884e51bf50f24559ceb3529f681f
maximovj <maximovj@outlook.com>
2024-05-19 00:23:22 -0600

    dev-maximo | *** bugfix | Modo Oscuro
    ** Se creo un nuevo componente hook modo oscuro

---
+++ dev-react-app/src/hooks/useModoOscuro.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+function useModoOscuro() {
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    const savedMode = localStorage.getItem("darkMode");
+    return savedMode ? JSON.parse(savedMode) : false;
+  });
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevMode) => {
+      const newMode = !prevMode;
+      localStorage.setItem("darkMode", JSON.stringify(newMode));
+      return newMode;
+    });
+  };
+
+  useEffect(() => {
+    document.body.classList.toggle("dark-mode", isDarkMode);
+
+    // Selecciona todos los elementos con la clase card
+    const cards = document.querySelectorAll(".card");
+    cards.forEach((card) => {
+      card.classList.toggle("card-dark-mode", isDarkMode);
+    });
+
+    // Selecciona todos los elementos con la clase btn
+    const btns = document.querySelectorAll(".btn");
+    btns.forEach((card) => {
+      card.classList.toggle("btn-dark-mode", isDarkMode);
+    });
+  }, [isDarkMode]);
+
+  return [isDarkMode, toggleDarkMode];
+}
+
+export default useModoOscuro;
