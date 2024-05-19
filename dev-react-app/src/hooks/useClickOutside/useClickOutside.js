230f48c7d0433dedc237f5c8bc0ad140d5582022
maximovj <maximovj@outlook.com>
2024-05-19 10:28:42 -0600

    dev-maximo | *** feature | Eliminar Hooks

--- dev-react-app/src/hooks/useClickOutside/useClickOutside.js
+++
@@ -1,8 +0,0 @@
-import useEventListener from "../useEventListener/useEventListener"
-
-export default function useClickOutside(ref, cb) {
-    useEventListener("click", e => {
-        if (ref.current == null || ref.current.contains(e.target)) return
-        cb(e)
-    }, document)
-}
