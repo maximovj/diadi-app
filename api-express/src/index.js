5c22fc55183dc77789534adfae5ad5e5443b67d3
maximovj <maximovj@outlook.com>
2024-05-25 17:09:10 -0600

    dev-maximo | *** Se agrego config para Sequelize y conexión a MySQL

--- api-express/src/index.js
+++ api-express/src/index.js
@@ -1,47 +1,43 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const { Sequelize } = require('sequelize');
-const db_env = require('./database');
+const { database_env, sequelize } = require('./sequelize');
 app.set('port', process.env.PORT || 3010);
-
-const sequelize = new Sequelize({
-    database: db_env.database,
-    username: db_env.username,
-    password: db_env.password,
-    port: db_env.port,
-    host: db_env.host,
-    dialect: db_env.connection,
-});
 
 app.get('/', function (request, response) {
     response.send('API de DIADI-APP');
 });
 
-const server = app.listen(app.get('port'), async function () {
-    const { address, port } = server.address();
-    const ip = address === '::' ? 'localhost' : address;
-    const protocol = 'http';
-    const url = `${protocol}://${ip}:${port}`;
 
+const iniciarServicio = async () => {
     try {
         await sequelize.authenticate();
-        console.log(
-            'Servidor corriendo exitosamente:' + '\n',
-            '::db_info::',
-            JSON.stringify(db_env, null, -2) + '\n',
-            '::status::',
-            JSON.stringify(server.address(), null, -2) + '\n',
-            '::url::',
-            url + '\n');
+        // force: true para borrar y crear de nuevo las tablas
+        await sequelize.sync({ force: false });
+
+        const server = app.listen(app.get('port'), async function () {
+            const { address, port } = server.address();
+            const ip = address === '::' ? 'localhost' : address;
+            const protocol = 'http';
+            const url = `${protocol}://${ip}:${port}`;
+            console.log(
+                'Servidor corriendo exitosamente:' + '\n',
+                '::db_info::',
+                JSON.stringify(database_env, null, -2) + '\n',
+                '::status::',
+                JSON.stringify(server.address(), null, -2) + '\n',
+                '::url::',
+                url + '\n');
+        });
+
     } catch (err) {
         console.log(
             'Error en la conexión a la base de datos:' + '\n',
             '::db_info::' + '\n',
-            JSON.stringify(db_env, null, 2) + '\n',
+            JSON.stringify(database_env, null, 2) + '\n',
             '::error::' + '\n',
             err + '\n');
-    } finally {
-        sequelize.close();
     }
-});
+}
+
+iniciarServicio();
