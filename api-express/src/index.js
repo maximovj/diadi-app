78fbd6962f11da1c830cd51048bf5c39a6ede056
maximovj <maximovj@outlook.com>
2024-05-25 15:11:03 -0600

    *** Se instala dotenv y se configura puerto para la API

--- api-express/src/index.js
+++ api-express/src/index.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const express = require('express');
-
 const app = express();
+app.set('puerto', process.env.APP_PUERTO || 3010);
 
 app.get('/', function (request, response){
     response.send('API de DIADI-APP');
 });
 
-app.listen(3000, function(){
-    console.log('Servidor ejecutandose');
+app.listen(app.get('puerto'), function(){
+    console.log('Servidor ejecutandose', app.get('puerto'));
 });
