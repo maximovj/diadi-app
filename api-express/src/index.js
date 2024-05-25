608822bf1f33db382da7e477d0b1201006d425ea
maximovj <maximovj@outlook.com>
2024-05-25 15:12:43 -0600

    *** Se modifica APP_PUERTO a PORT

--- api-express/src/index.js
+++ api-express/src/index.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-app.set('puerto', process.env.APP_PUERTO || 3010);
+app.set('port', process.env.PORT || 3010);
 
 app.get('/', function (request, response){
     response.send('API de DIADI-APP');
 });
 
-app.listen(app.get('puerto'), function(){
-    console.log('Servidor ejecutandose', app.get('puerto'));
+app.listen(app.get('port'), function(){
+    console.log('Servidor ejecutandose', app.get('port'));
 });
