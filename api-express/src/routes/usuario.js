91879104b8f8638d68e906ef9c42e593587c4396
maximovj <maximovj@outlook.com>
2024-05-25 18:26:13 -0600

    *** Se agrega ruta para ver usuario

--- api-express/src/routes/usuario.js
+++ api-express/src/routes/usuario.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router();
 const Usuario = require('../models/Usuario');
 
+// Esta ruta es para ver los datos de un usuario registrado
+router.get('/usuario/:id', async (req, res) => {
+    const { id } = req.params;
+    try{
+        const usuario_ver = await Usuario.findOne({where: { id }});
+        res.status(201).json(usuario_ver);
+    }catch(err){
+        res.status(404).json({ error: err.errors[0] });
+    }
+});
+
 // Esta ruta es para crear o registrar un nuevo usuario
 router.post('/usuario', async (req, res) => {
     const { usuario, correo, contrasena } = req.body;
@@ -9,7 +20,7 @@ router.post('/usuario', async (req, res) => {
         const registrar_usuario = await Usuario.create({ usuario, correo, contrasena });
         res.status(201).json(registrar_usuario);
     }catch(err){
-        res.status(404).json({ error: err.message });
+        res.status(404).json({ error: err.message[0] });
     }
 });
 
