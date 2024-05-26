22fc8aae07bbf1d7742298bd15799a1f7acb77f4
maximovj <maximovj@outlook.com>
2024-05-25 18:32:39 -0600

    *** Se agrega ruta para listar 15 usuarios

--- api-express/src/routes/usuario.js
+++ api-express/src/routes/usuario.js
@@ -2,6 +2,18 @@ const express = require('express');
 const router = express.Router();
 const Usuario = require('../models/Usuario');
 
+// @see https://sequelize.org/docs/v6/core-concepts/model-querying-basics/#limits-and-pagination
+// @see https://sequelize.org/docs/v6/core-concepts/model-querying-basics/#simple-select-queries
+// Esta ruta es para listar todos los usuarios registrados
+router.get('/usuario', async (req, res) => {
+    try{
+        const usuario_listar = await Usuario.findAll({limit: 15});
+        res.status(201).json(usuario_listar);
+    }catch(err){
+        res.status(404).json({ error: err.errors[0] });
+    }
+});
+
 // Esta ruta es para ver los datos de un usuario registrado
 router.get('/usuario/:id', async (req, res) => {
     const { id } = req.params;
