a945a05da3bc96232d67c3fdb66065200bec4f82
maximovj <maximovj@outlook.com>
2024-05-25 18:15:17 -0600

    *** Se agrega ruta para eliminar usuario

--- api-express/src/routes/usuario.js
+++ api-express/src/routes/usuario.js
@@ -15,11 +15,22 @@ router.post('/usuario', async (req, res) => {
 
 // Esta ruta es para editar o modificar un usuario registrado
 router.put('/usuario/:id', async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     const { usuario, correo, contrasena } = req.body;
     try{
         const modificar_usuario = await Usuario.update({ usuario, correo, contrasena }, { where: { id } });
         res.status(201).json(modificar_usuario);
+    }catch(err){
+        res.status(404).json({ error: err.errors[0] });
+    }
+});
+
+// Esta ruta es para eliminar un usuario registrado
+router.delete('/usuario/:id',  async (req, res) => {
+    const { id } = req.params;
+    try{
+        const eliminar_usuario = await Usuario.destroy({ where: { id } });
+        res.status(201).json(eliminar_usuario);
     }catch(err){
         res.status(404).json({ error: err.errors[0] });
     }
