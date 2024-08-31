4cf66e649c9af408023ea06ef9c71535fc6c1340
maximovj <maximovj@outlook.com>
2024-08-31 15:08:03 -0600

    Se agrega index.js para iniciar la API y app.js para desarrollar el API

---
+++ api-express/src/app.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const sequelizeConfig = require('./config/sequelizeConfig.js');
+const corsConfig = require('./config/corsConfig.js');
+const express = require('express');
+const app = express();
+
+// Importar modelos para que las relaciones se definan
+require('./models/diarioModel.js');
+require('./models/usuarioModel.js');
+require('./models/tareaModel.js');
+
+// Importar las rutas
+const rutas_usuario = require('./routes/usuarioRoutes.js');
+const rutas_diario = require('./routes/diarioRoutes.js');
+const rutas_tarea = require('./routes/tareaRoutes.js');
+
+// Se define variables de entorno
+app.set('port', process.env.PORT || 3010);
+app.set('log', process.env.LOG || true);
+
+// Middleware para parsear JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Se configura CORS
+app.use(corsConfig);
+
+// Usar las rutas
+app.use('/api/v1', rutas_usuario);
+app.use('/api/v1', rutas_diario);
+app.use('/api/v1', rutas_tarea);
+
+sequelizeConfig.sync({ alter: false, force: false });
+
+module.exports = app;
