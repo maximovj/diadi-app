a36a5cab04c0d2d2a0b4e0f6660b607d62dc995a
maximovj <maximovj@outlook.com>
2024-08-31 14:28:01 -0600

    Se distribuye lógica para tareas hacia un controlador

---
+++ api-express/src/controller/TareaController.js
@@ -0,0 +1,63 @@
+const tarea = require('../models/Tarea.js');
+
+exports.listarTareas = async (req, res) => {
+    const tareas_15 = await tarea.findAll({ limit: 15 });
+    res.status(200).json({ test: 'Endpoint /GET', tareas_15 });
+};
+
+exports.verTarea = async (req, res) => {
+    const id = req.params.id;
+    const buscar_tarea = await tarea.findByPk(id);
+
+    if (buscar_tarea) {
+        res.status(200).json({ test: 'Endpoint /GET', buscar_tarea });
+    } else {
+        return res.status(404).json({ test: 'Tarea no encontrada' });
+    }
+};
+
+exports.crearTarea = async (req, res) => {
+    const { titulo, descripcion, estado, importancia, fecha_inicio, fecha_limite } = req.body;
+    const crear_tarea = await tarea.create({
+        titulo,
+        descripcion,
+        estado,
+        importancia,
+        fecha_inicio,
+        fecha_limite
+    });
+    res.status(200).json({ test: 'Endpoint /GET', crear_tarea });
+};
+
+exports.modificarTarea = async (req, res) => {
+    const { titulo, descripcion, estado, importancia, fecha_inicio, fecha_limite } = req.body;
+    const id = req.params.id;
+
+    const buscar_tarea = await tarea.findByPk(id);
+
+    if (buscar_tarea) {
+        const actualizar_tarea = await buscar_tarea.update({
+            titulo,
+            descripcion,
+            estado,
+            importancia,
+            fecha_inicio,
+            fecha_limite
+        });
+        return res.status(200).json({ test: 'Endpoint /GET', actualizar_tarea });
+    } else {
+        return res.status(404).json({ test: 'tarea no encontrada' });
+    }
+};
+
+exports.eliminarTarea = async (req, res) => {
+    const id = req.params.id;
+    const buscar_tarea = await tarea.findByPk(id);
+
+    if (buscar_tarea) {
+        await buscar_tarea.destroy();
+        return res.status(200).json({ test: 'Tarea eliminado' });
+    } else {
+        return res.status(404).json({ test: 'Tarea no encontrada' });
+    }
+};
--- api-express/src/routes/tarea.js
+++ api-express/src/routes/tarea.js
@@ -1,73 +1,25 @@
 const express = require('express');
 const routers = express.Router();
-const tarea = require('../models/Tarea.js');
+const {
+    crearTarea,
+    eliminarTarea,
+    listarTareas,
+    modificarTarea,
+    verTarea } = require('../controller/TareaController.js');
 
 // Listar 15 tareas
-routers.get('/tarea', async (req, res) => {
-    const tareas_15 = await tarea.findAll({ limit: 15 });
-    res.status(200).json({ test: 'Endpoint /GET', tareas_15 });
-});
+routers.get('/tarea', listarTareas);
 
 // Ver información de una tarea
-routers.get('/tarea/:id', async (req, res) => {
-    const id = req.params.id;
-    const buscar_tarea = await tarea.findByPk(id);
-
-    if (buscar_tarea) {
-        res.status(200).json({ test: 'Endpoint /GET', buscar_tarea });
-    } else {
-        return res.status(404).json({ test: 'Tarea no encontrada' });
-    }
-});
+routers.get('/tarea/:id', verTarea);
 
 // Registrar una tarea
-routers.post('/tarea', async (req, res) => {
-    const { titulo, descripcion, estado, importancia, fecha_inicio, fecha_limite } = req.body;
-    const crear_tarea = await tarea.create({
-        titulo,
-        descripcion,
-        estado,
-        importancia,
-        fecha_inicio,
-        fecha_limite
-    });
-    res.status(200).json({ test: 'Endpoint /GET', crear_tarea });
-});
+routers.post('/tarea', crearTarea);
 
 // Actualizar una tarea
-routers.put('/tarea/:id', async (req, res) => {
-    const { titulo, descripcion, estado, importancia, fecha_inicio, fecha_limite } = req.body;
-    const id = req.params.id;
-
-    const buscar_tarea = await tarea.findByPk(id);
-
-    if (buscar_tarea) {
-        const actualizar_tarea = await buscar_tarea.update({
-            titulo,
-            descripcion,
-            estado,
-            importancia,
-            fecha_inicio,
-            fecha_limite
-        });
-        return res.status(200).json({ test: 'Endpoint /GET', actualizar_tarea });
-    } else {
-        return res.status(404).json({ test: 'tarea no encontrada' });
-    }
-});
+routers.put('/tarea/:id', modificarTarea);
 
 //  Eliminar una tarea
-routers.delete('/tarea/:id', async (req, res) => {
-    const id = req.params.id;
-    const buscar_tarea = await tarea.findByPk(id);
-
-    if (buscar_tarea) {
-        await buscar_tarea.destroy();
-        return res.status(200).json({ test: 'Tarea eliminado' });
-    } else {
-        return res.status(404).json({ test: 'Tarea no encontrada' });
-    }
-});
-
+routers.delete('/tarea/:id', eliminarTarea);
 
 module.exports = routers;
