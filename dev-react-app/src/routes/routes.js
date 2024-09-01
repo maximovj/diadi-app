45d1f9c90456b14879179f8a642e7f41bd453dd3
maximovj <maximovj@outlook.com>
2024-08-31 19:15:32 -0600

    Se agrega ruta públicas y protegidas

--- dev-react-app/src/routes/routes.js
+++ dev-react-app/src/routes/routes.js
@@ -6,22 +6,29 @@ import { Diarios } from '../pages/Diarios'
 import { Tareas } from '../pages/Tareas'
 import { NavBar } from '../components/NavBar'
 import { Pie } from '../components/Pie'
+import { Panel } from '../pages/Panel'
+import { RutaProtegida } from '../components/RutaProtegida'
+import { RutaPublica } from '../components/RutaPublica'
 
-export function RoutePage (){
-    return(
+export function RoutePage() {
+    return (
         <BrowserRouter>
-            <NavBar></NavBar>
+            <NavBar />
             <div className='container container-css my-4'>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/inicio" element={<Home/>}/>
-                <Route path="/acceder" element={<Acceder/>}/>
-                <Route path="/registrarme" element={<Registrarme/>}/>
-                <Route path="/diarios" element={<Diarios/>}/>
-                <Route path="/tareas" element={<Tareas/>}/>
-            </Routes>
+                <Routes>
+                    {/* Rutas públicas */}
+                    <Route path="/" element={<RutaPublica><Home /></RutaPublica>} />
+                    <Route path="/inicio" element={<RutaPublica><Home /></RutaPublica>} />
+                    <Route path="/acceder" element={<RutaPublica><Acceder /></RutaPublica>} />
+                    <Route path="/registrarme" element={<RutaPublica><Registrarme /></RutaPublica>} />
+
+                    {/* Rutas protegidas */}
+                    <Route path="/panel" element={<RutaProtegida><Panel /></RutaProtegida>} />
+                    <Route path="/diarios" element={<RutaProtegida><Diarios /></RutaProtegida>} />
+                    <Route path="/tareas" element={<RutaProtegida><Tareas /></RutaProtegida>} />
+                </Routes>
             </div>
-            <Pie></Pie>
-        </BrowserRouter>   
-    )
+            <Pie />
+        </BrowserRouter>
+    );
 }
