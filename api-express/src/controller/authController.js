3f5a0d249fdb01146ea560b8ee86b09eb5cb8d8c
maximovj <maximovj@outlook.com>
2024-08-31 18:35:08 -0600

    Se modifica la ruta y controlador para auth (autenticación)

--- api-express/src/controller/authController.js
+++ api-express/src/controller/authController.js
@@ -4,7 +4,14 @@ exports.registrar = (req, res) => {
     const { usuario, correo, contrasena } = req.body;
     Usuario.create({ usuario, correo, contrasena })
         .then(() => res.status(201).json({ cxt_contenido: 'Usuario registrado', success: true, }))
-        .catch((err) => res.status(500).json({ cxt_contenido: err.message, success: false, }));
+        .catch((err) => {
+            console.log(Object.keys(err), err.name, err.origina, err.sql, err.errors);
+            if (err.name === 'SequelizeUniqueConstraintError') {
+                return res.status(409).json({ ctx_contenido: 'El usuario ya esta registrado', success: false, });
+            } else {
+                return res.status(500).json({ ctx_contenido: err.message, success: false, });
+            }
+        });
 };
 
 exports.acceder = (req, res) => {
@@ -13,12 +20,12 @@ exports.acceder = (req, res) => {
         .then((data) => {
             if (data.contrasena === contrasena) {
                 return res.status(200).json({
-                    cxt_contenido: 'Usuario inicio sesión correctamente.', success: true,
+                    ctx_contenido: 'Usuario inicio sesión correctamente.', success: true,
                     data: { id: data.id, correo: data.correo }
                 });
             } else {
-                return res.status(401).json({ cxt_contenido: 'Credenciales incorrectas.', success: false, data: null });
+                return res.status(401).json({ ctx_contenido: 'Credenciales incorrectas.', success: false, data: null });
             }
         })
-        .catch(() => res.status(500).json({ cxt_contenido: err.message, success: false, data: null }));
+        .catch(() => res.status(500).json({ ctx_contenido: err.message, success: false, data: null }));
 };
--- api-express/src/routes/authRoutes.js
+++ api-express/src/routes/authRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const { registrar, acceder } = require('../controller/authController.js');
 
 // Ruta para registrar un nuevo usuario
-router.post('/registrar', registrar);
+router.post('/auth/registrar', registrar);
 
 // Ruta para acceder al sistema con un usuario
-router.post('/acceder', acceder);
+router.post('/auth/acceder', acceder);
 
 module.exports = router;
